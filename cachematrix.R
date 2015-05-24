6af6dc9f2b4538aeecab57b4ff124ec398f08045
Tom Smith <thehumbledba@gmail.com>
2015-05-24 09:43:12 -0700

    final

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-## TJ Smith 5/18/2015
-
+# Exercise in cache functions using lexical scoping
+# Tom Smith 5/18/2015
+# Usage:  
+    # Construct a function to take in a square matrix x
+    # Call makeCacheMatrix(x), assign result to variable
+    # Call cacheSolve (variable) to return the inverse, either calculated or from cache
+    
 makeCacheMatrix <- function(x = matrix()) {
+    # set up the cache environment
+    # x: a square matrix
+    # return: a list containing functions to:
+            #set the incoming matrix
+            #get the incoming matrix
+            #set the matrix inverse
+            #get the matrix inverse
+        inv = NULL
+        set = function(y) {
+                x <<- y
+                inv <- NULL
+        }
+        get = function() x
+        setinv = function(inverse) inv <- inverse 
+        getinv = function() inv
+        list(set=set, get=get, setinv=setinv, getinv=getinv)
 
 }
 
 
-## Write a short comment describing this function
-## TJ Smith 5/18/2015
+## Tom Smith 5/18/2015
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    #set up the solve envronment
+    #return the inverse of 'x'
+
+        inv = x$getinv()
+        
+        # check if inverse exists
+        if (!is.null(inv)){
+                # get from the cache 
+                return(inv)
+        }
+        
+        # if inverse not exists:
+        mat.data = x$get()
+        inv = solve(mat.data, ...)
+        
+        # sets the value of the inverse in the cache
+        x$setinv(inv)
+        
+        return(inv)
 }
+
+
